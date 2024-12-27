2ec19a5faa5a433461905b833f9a0ae3a6b8d598
Lewis Benson <tm.lewisbenson@gmail.com>
2024-12-27 15:44:34 -0500

    build player class with draw method

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 def main():
   pygame.init()
   clock = pygame.time.Clock()
   dt = 0
-
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT / 2
+  player = Player(x,y)
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  
   while True:
     screen.fill("black")
+    player.draw(screen)
+
+
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return
@@ -16,9 +23,7 @@ def main():
     dt /= 1000 # convert to millisecond
 
     pygame.display.flip()# last
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
   main()
