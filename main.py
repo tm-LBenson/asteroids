445271c52e67ab331358bec73e847584a6fdac78
Lewis Benson <tm.lewisbenson@gmail.com>
2024-12-27 14:31:43 -0500

    add max framerate

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import pygame
 from constants import *
 def main():
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0
+
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   while True:
     screen.fill("black")
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return
+
+    dt = clock.tick(MAX_FRAME_RATE)
+    dt /= 1000 # convert to millisecond
 
     pygame.display.flip()# last
   print("Starting asteroids!")
