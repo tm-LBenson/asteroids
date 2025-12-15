96b6d8d3857c967b21908899fc7595ab8cb6b404
Lewis Benson <tm.lewisbenson@gmail.com>
2025-12-15 00:35:34 -0500

    setup with 60 fps

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen.fill("black")
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
