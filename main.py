2695d4835b56ccd87e7e55f3021b6c75d2c9123a
Lewis Benson <tm.lewisbenson@gmail.com>
2025-12-16 22:04:45 -0500

    add movement

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen.fill("black")
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
+        screen.fill("black")
+
         player.draw(screen)
         player.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from circleshape import CircleShape
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED,PLAYER_SPEED
 
 
 class Player (CircleShape):
@@ -15,12 +15,18 @@ class Player (CircleShape):
       keys = pygame.key.get_pressed()
 
       if keys[pygame.K_a]:
-          self.rotate(dt)
-      if keys[pygame.K_d]:
           self.rotate(-dt)
-
+      if keys[pygame.K_d]:
+          self.rotate(dt)
+      if keys[pygame.K_w]:
+          self.move(dt)
+      if keys[pygame.K_s]:
+          self.move(-dt)
   def move(self, dt):
-      pass
+    unit_vector = pygame.Vector2(0, 1)
+    rotated_vector = unit_vector.rotate(self.rotation)
+    rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+    self.position += rotated_with_speed_vector
 
   def triangle(self):
       forward = pygame.Vector2(0, 1).rotate(self.rotation)
